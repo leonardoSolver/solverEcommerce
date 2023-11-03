2971328d002855d2671f5fb64cee2bfd33a9f830
Leonardo Almeida <leonardo16almeida@gmail.com>
2023-11-03 09:52:06 -0300

    feat(routes): configure two main routes to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './modules/login/components/login-form/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: "", redirectTo: "/home", pathMatch: "full" }, {path: "home", component: LoginFormComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
